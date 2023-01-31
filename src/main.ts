ca3d73385d9d5b2ea8e3fbd7f9934478d4b1593d
andre.teixeira <andre-x8@hotmail.com>
2023-01-31 09:02:57 -0300

    Adicionado class-validator e class-transforme

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())/**Import para as validaçoes do DTO funcionarem */
   await app.listen(3000);
   console.log("Servidor rodando na porta 3000")
 }
