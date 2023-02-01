dba14ad43a7456eeb98639d8898f7176570651de
andre.teixeira <andre-x8@hotmail.com>
2023-02-01 09:06:40 -0300

    Criado o metodo create e já salvando no  banco de dados

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+import { PrismaModule } from "src/prisma/prisma.module";
 import { UserController } from "./user.controller";
+import { UserService } from "./user.service";
 
 @Module({
-    imports:[],/** Aqui vai os modulos que ele vai importar, receber outros modulos */
+    imports:[PrismaModule],/** Aqui vai os modulos que ele vai importar, receber outros modulos */
     controllers: [UserController],/**Controllers disponibilizados por esse modulo */
-    providers:[], /**Quais são os serviços que poderão ser injetados ou providos pelo modulo */
+    providers:[UserService], /**Quais são os serviços que poderão ser injetados ou providos pelo modulo */
     exports:[],/**Quais os recursos que eu tenho no module que eu quero exportar para outros modulos  */
 })
 export class UserModule{}
