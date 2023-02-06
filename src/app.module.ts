3cfc623f871e1acf27485a2f9f65361a918f460d
andre.teixeira <andre-x8@hotmail.com>
2023-02-06 09:32:50 -0300

    Adicionado serviço de envio de e-mail

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { MailerModule } from '@nestjs-modules/mailer';
 import { forwardRef, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
@@ -5,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import {PugAdapter} from '@nestjs-modules/mailer/dist/adapters/pug.adapter'
 
 @Module({
   imports: [ConfigModule.forRoot(),
@@ -14,7 +16,21 @@ import { UserModule } from './user/user.module';
     limit:10 //quantos acessos por minuto a aplicação pode receber
 
   }),
-     forwardRef(()=>UserModule) ,forwardRef(()=>AuthModule) ],/** Aqui vai os modulos que ele vai importar, receber outros modulos */
+     forwardRef(()=>UserModule) ,forwardRef(()=>AuthModule),
+      MailerModule.forRoot({
+        transport:'smtps://conner9@ethereal.email:v39aSvjMp3KG877Auv@smtp.ethereal.email',
+        defaults:{
+          from:'',
+        },
+        template: {
+          dir: __dirname + '/template',
+          adapter: new PugAdapter(),
+          options:{
+            strict:true,
+          },
+        }
+      })
+    ],/** Aqui vai os modulos que ele vai importar, receber outros modulos */
   controllers: [AppController],/**Controllers disponibilizados por esse modulo */
   providers: [AppService], /**Quais são os serviços que poderão ser injetados ou providos pelo modulo */
   exports:[AppService] /**Quais os recursos que eu tenho no module que eu quero exportar para outros modulos  */
