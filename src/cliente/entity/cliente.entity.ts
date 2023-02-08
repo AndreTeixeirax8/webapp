06d2e84743ee3bd747df4b787954aa3d885324f3
andre.teixeira <andre-x8@hotmail.com>
2023-02-08 20:58:36 -0300

    Criado entidade de clientes

---
+++ src/cliente/entity/cliente.entity.ts
@@ -0,0 +1,39 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('cliente')
+export class ClienteEntity{
+
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    name:string;
+
+    @Column()
+    email:string;
+
+    @Column({
+        type:'date',
+        nullable:true,
+    })
+    birthAt:Date;
+
+    @CreateDateColumn()
+    createdAt:string;
+
+    @UpdateDateColumn()
+    updatedAt:string;
+
+    @Column({nullable: true})
+    cidade:number;
+
+    @Column({nullable: true})
+    unidadeFederal:number;
+
+    @Column({nullable: true})
+    telefone:string;
+
+    @Column()
+    cpf:string;
+
+}
