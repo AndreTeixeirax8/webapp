8ff5a7190a14d2c399ab051f29c85119554a9faa
andre.teixeira <andre-x8@hotmail.com>
2023-02-08 20:55:01 -0300

    Criado DTO de clientes

---
+++ src/cliente/dtos/criar-cliente.dto.ts
@@ -0,0 +1,28 @@
+import { IsDateString, IsEmail, IsInt, IsOptional, IsString } from "class-validator";
+
+export class  CriarClienteDTO{
+    @IsString()
+    name:string;
+
+    @IsEmail()
+    email:string;
+ 
+    @IsOptional()
+    @IsDateString()
+    birthAt: string;
+
+    @IsInt()
+    cidade:number;
+
+    @IsInt()
+    unidadeFederal:number;
+
+    @IsString()
+    telefone:string;
+
+    @IsString()
+    cpf:string;
+
+
+    
+}
