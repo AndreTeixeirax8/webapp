e50d0535183dadba4f032a66d474afae991eb1b9
andre.teixeira <andre-x8@hotmail.com>
2023-02-08 21:31:15 -0300

    Adicionado o metodo de  criar no controller cliente

--- src/cliente/cliente.controller.ts
+++ src/cliente/cliente.controller.ts
@@ -1,7 +1,11 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "src/guards/auth.guard";
+import { RoleGuard } from "src/guards/role.guard";
 import { ClienteService } from "./cliente.service";
 import {CriarClienteDTO} from "./dtos/criar-cliente.dto"
 
+//@UserGuards(ThrottlerGuard({})) usar para proteger a aplicação cotnra DDOS
+@UseGuards(AuthGuard, RoleGuard)//não inverter as ordens porem o ThrottlerGuard tem que vir primeiro para proteger a api 
 @Controller('clientes')
 export class ClienteController{
 
--- src/cliente/cliente.module.ts
+++ src/cliente/cliente.module.ts
@@ -1,12 +1,13 @@
 import { forwardRef, Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthModule } from "src/auth/auth.module";
+import { UserModule } from "src/user/user.module";
 import { ClienteController } from "./cliente.controller";
 import { ClienteService } from "./cliente.service";
 import { ClienteEntity } from "./entity/cliente.entity";
 
 @Module({
-    imports:[forwardRef(() => AuthModule),
+    imports:[forwardRef(() => AuthModule),forwardRef(()=>UserModule),
         TypeOrmModule.forFeature([ClienteEntity]) ],/** Aqui vai os modulos que ele vai importar, receber outros modulos */
     controllers: [ClienteController],/**Controllers disponibilizados por esse modulo */
     providers:[ClienteService], /**Quais são os serviços que poderão ser injetados ou providos pelo modulo */
