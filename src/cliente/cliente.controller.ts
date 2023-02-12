7bfb22f094d9299228c83c34deca91834c95a296
andre.teixeira <andre-x8@hotmail.com>
2023-02-11 21:03:37 -0300

    Configurado o Prettier

--- src/cliente/cliente.controller.ts
+++ src/cliente/cliente.controller.ts
@@ -1,39 +1,40 @@
-import { Body, Controller, Get, Param, Post, Query, UseGuards } from "@nestjs/common";
-import { Roles } from "src/decorators/roles.decorator";
-import { Role } from "src/enums/role.enum";
-import { AuthGuard } from "src/guards/auth.guard";
-import { RoleGuard } from "src/guards/role.guard";
-import { ClienteService } from "./cliente.service";
-import {CriarClienteDTO} from "./dtos/criar-cliente.dto"
-import { ClienteEntity } from "./entity/cliente.entity";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { RoleGuard } from 'src/guards/role.guard';
+import { ClienteService } from './cliente.service';
+import { CriarClienteDTO } from './dtos/criar-cliente.dto';
+import { ClienteEntity } from './entity/cliente.entity';
 
 //@UserGuards(ThrottlerGuard({})) usar para proteger a aplicação cotnra DDOS
-@UseGuards(AuthGuard, RoleGuard)//não inverter as ordens porem o ThrottlerGuard tem que vir primeiro para proteger a api 
+@UseGuards(AuthGuard, RoleGuard) //não inverter as ordens porem o ThrottlerGuard tem que vir primeiro para proteger a api
 @Controller('clientes')
-export class ClienteController{
+export class ClienteController {
+  constructor(private readonly clienteService: ClienteService) {}
 
-    constructor(
-        private readonly clienteService:ClienteService,
-    ){}
+  @Roles(Role.Admin)
+  @Post()
+  async create(@Body() body: CriarClienteDTO) {
+    return this.clienteService.criar(body);
+  }
 
-    @Roles(Role.Admin)    
-    @Post()
-    async create(@Body() body:CriarClienteDTO){
-        return this.clienteService.criar(body); 
-    }
+  @Roles(Role.Admin)
+  @Get()
+  async list() {
+    return this.clienteService.buscaTodos();
+  }
 
-    @Roles(Role.Admin)
-    @Get()
-    async list(){
-        return this.clienteService.buscaTodos();
-    }
-
-    
-    @Get(':nome')
-   async show(@Param('nome') nome :string){ 
-
-       return this.clienteService.show(nome);
-    }
-
-
+  @Get(':nome')
+  async show(@Param('nome') nome: string) {
+    return this.clienteService.show(nome);
+  }
 }
--- src/cliente/cliente.service.ts
+++ src/cliente/cliente.service.ts
@@ -1,48 +1,48 @@
-import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Like, Repository } from "typeorm";
-import { ClienteEntity } from "./entity/cliente.entity";
-import {CriarClienteDTO} from "./dtos/criar-cliente.dto"
-import { ClienteErrorApiEnum } from "src/common/enums/error-api.enum";
-
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Like, Repository } from 'typeorm';
+import { ClienteEntity } from './entity/cliente.entity';
+import { CriarClienteDTO } from './dtos/criar-cliente.dto';
+import { ClienteErrorApiEnum } from 'src/common/enums/error-api.enum';
 
 @Injectable()
-export class ClienteService{
-    constructor(
-        @InjectRepository(ClienteEntity)
-        private readonly clienteRepository:Repository<ClienteEntity>
-        ){}
+export class ClienteService {
+  constructor(
+    @InjectRepository(ClienteEntity)
+    private readonly clienteRepository: Repository<ClienteEntity>
+  ) {}
 
-        async criar(data: CriarClienteDTO) {
-            if (
-              await this.clienteRepository.exist({
-                where: {
-                  email: data.email,
-                },
-              })
-            ) {
-              throw new BadRequestException('Este e-mail já está sendo usado.');
-            }
-        
-       
-            const user = this.clienteRepository.create(data);
-        
-            return this.clienteRepository.save(user);
-          }
+  async criar(data: CriarClienteDTO) {
+    if (
+      await this.clienteRepository.exist({
+        where: {
+          email: data.email,
+        },
+      })
+    ) {
+      throw new BadRequestException('Este e-mail já está sendo usado.');
+    }
 
-          async buscaTodos() {
-            return this.clienteRepository.find();
-          }
+    const user = this.clienteRepository.create(data);
 
-          async show(nome: string) {
+    return this.clienteRepository.save(user);
+  }
 
-            const busca =  await this.clienteRepository.find({
-                 where:[{name: Like(`%${nome}%`)}]
-               });
-            
-            if (busca.length==0 ) throw new NotFoundException(ClienteErrorApiEnum.NaoEncotrado);
-            return busca ;
-          }
+  async buscaTodos() {
+    return this.clienteRepository.find();
+  }
 
+  async show(nome: string) {
+    const busca = await this.clienteRepository.find({
+      where: [{ name: Like(`%${nome}%`) }],
+    });
 
+    if (busca.length == 0)
+      throw new NotFoundException(ClienteErrorApiEnum.NaoEncotrado);
+    return busca;
+  }
 }
